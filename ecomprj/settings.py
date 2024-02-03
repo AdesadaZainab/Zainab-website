959b2d879879dadef4dea800686bf6b09bbf6c1c
smilegbolahan <42694929+smilegbolahan@users.noreply.github.com>
2024-02-03 23:22:58 +0100

    last2 commit

--- ecomprj/settings.py
+++ ecomprj/settings.py
@@ -28,9 +28,7 @@ DEBUG = True
 
 ALLOWED_HOSTS = ["*"]
 
-CSRF_TRUSTED_ORIGINS = ['https://web-production-c392.up.railway.app/user/sign-in/',
-                        'https://web-production-c392.up.railway.app/contact',
-                         'https://web-production-c392.up.railway.app/user/sign-up/' ]
+CSRF_TRUSTED_ORIGINS = ['https://web-production-c392.up.railway.app']
 
 
 # Application definition
