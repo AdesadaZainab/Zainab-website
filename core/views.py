58b4b581e7a4bc5442af637863ad1388243462dc
smilegbolahan <42694929+smilegbolahan@users.noreply.github.com>
2024-02-04 09:34:58 +0100

    last2 commit

--- core/views.py
+++ core/views.py
@@ -28,7 +28,7 @@ def index(request):
         "products":products
     }
 
-    return render(request, 'core//', context)
+    return render(request, 'core/index.html', context)
 
 
 def product_list_view(request):
